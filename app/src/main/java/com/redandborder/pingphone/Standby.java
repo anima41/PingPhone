2125e3649a54a435c01701ed0640a75f75867a39
tesoro01 <aqaqua01@gmail.com>
2015-10-08 14:41:29 +0900

    タップ時背景色変更、時計追加

--- app/src/main/java/com/redandborder/pingphone/Standby.java
+++ app/src/main/java/com/redandborder/pingphone/Standby.java
@@ -1,13 +1,16 @@
 package com.redandborder.pingphone;
 
 import android.app.Activity;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -15,13 +18,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class Standby extends Activity {
+public class Standby extends Activity implements OnClickListener {
 
-/*    private Handler mHandler;
+    private Handler mHandler;
     private Timer mTimer;
     // time format
     private static SimpleDateFormat mSimpleDataFormat = new SimpleDateFormat("yyyy年　MM月dd日　HH:mm:ss");
-*/
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,35 +34,59 @@ public class Standby extends Activity {
         // keep screen
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-/*            mHandler = new Handler(getMainLooper());
-            mTimer = new Timer();
+        // taskbar nashi
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-            //1s gotoni run
-            mTimer.schedule(new TimerTask() {
+        // button set
+        Button btn = (Button) findViewById(R.id.skype_button); //ToDO koko ni kaite iinoka
+        btn.setOnClickListener(this);
 
-                @Override
-                public void run() {
-                    mHandler.post(new Runnable() {
-                        public void run() {
-                            Calendar calendar = Calendar.getInstance();
-                            String nowDate = mSimpleDataFormat.format(calendar.getTime());
-                            // TextView shutoku
-                            ((TextView) findViewById(R.id.standby_time)).setText(nowDate);
-                        }
-                    });}
-            },0,1000);
-        }
 
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            // gamen ga kawaru toki cancel
-            if (mTimer != null) {
-                mTimer.cancel();
-                mTimer = null;
+        mHandler = new Handler(getMainLooper());
+        mTimer = new Timer();
+
+        //1s gotoni run
+        mTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                mHandler.post(new Runnable() {
+                    public void run() {
+                        Calendar calendar = Calendar.getInstance();
+                        String nowDate = mSimpleDataFormat.format(calendar.getTime());
+                        // TextView shutoku
+                        TextView watchText = (TextView) findViewById(R.id.standby_watch);
+                        watchText.setText(nowDate);
+                        // TextView ichi
+                        watchText.setGravity(Gravity.CENTER_HORIZONTAL);
+                    }
+                });
             }
-*/
+        }, 0, 1000);
+    }
+
+    public void onClick(View v) {
+        // button set
+        Button btn = (Button) findViewById(R.id.skype_button); //ToDo muda deha?
+        //layout set
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.standby_layout);
+
+        if (v == btn) {
+            layout.setBackgroundColor(Color.DKGRAY);
         }
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // gamen ga kawaru toki cancel
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
+
+    }
+
+}
 
