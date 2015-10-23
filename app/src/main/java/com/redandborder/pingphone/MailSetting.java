838197e67b9d7c58634d38378f41723bc4d9aebf
tesoro01 <aqaqua01@gmail.com>
2015-10-23 17:50:21 +0900

    レイアウト調整

--- app/src/main/java/com/redandborder/pingphone/MailSetting.java
+++ app/src/main/java/com/redandborder/pingphone/MailSetting.java
@@ -1,38 +1,49 @@
 package com.redandborder.pingphone;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MailSetting extends ActionBarActivity {
+public class MailSetting extends ActionBarActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_setting);
+
+        Button maSet = (Button)findViewById(R.id.mail_set_button);
+        maSet.setOnClickListener(this);
+
+        Button maSkip = (Button)findViewById(R.id.mail_skip_button);
+        maSkip.setOnClickListener(this);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mail_setting, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mail_set_button:
+                //db hozon
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                Intent intent1 = new Intent(MailSetting.this, Standby.class);
+                startActivity(intent1);
+                MailSetting.this.finish();
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.mail_skip_button:
+                Intent intent2 = new Intent(MailSetting.this, Standby.class);
+                startActivity(intent2);
+                MailSetting.this.finish();
+                break;
+
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
