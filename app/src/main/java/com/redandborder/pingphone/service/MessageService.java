53a5b6f7cc8c57ea5444cb1f9915ace8a487624f
tesoro01 <aqaqua01@gmail.com>
2015-10-26 23:10:18 +0900

    Notification compat builderを使った

--- app/src/main/java/com/redandborder/pingphone/service/MessageService.java
+++ app/src/main/java/com/redandborder/pingphone/service/MessageService.java
@@ -1,48 +1,62 @@
 package com.redandborder.pingphone.service;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.redandborder.pingphone.R;
-import com.redandborder.pingphone.Standby;
 
 import java.util.Random;
 
 
 public class MessageService extends Service{
-    NotificationManager notificationManager;
-    Random randomMessage;
+   // NotificationManager notificationManager;
+   NotificationManagerCompat manager = null;
 
-    String[] message ={"サービス実行中です",
-                        "引き続きサービスを実行しています"};
+    //Random randomMessage;
+
+    //String[] message ={"サービス実行中です",
+    //                    "引き続きサービスを実行しています"};
+
+    private static final int NOTIFICATION_MINIMUM_ID = 001;
+
+
 
     public void onCreate(){
-        notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
-        randomMessage = new Random();
+        //notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+        //randomMessage = new Random();
     }
 
-    public void onStart(Intent it,int id){
-        Notification notification = new Notification(R.mipmap.ic_launcher,"Service is running",System.currentTimeMillis());
+    @Override
+    public int onStartCommand(Intent it,int id,int startId){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(NOTIFICATION_MINIMUM_ID,builder.build());
 
-        Intent intent = new Intent(this, Standby.class);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        //Notification notification = new Notification(R.mipmap.ic_launcher,"Service is running",System.currentTimeMillis());
 
-        notification.setLatestEventInfo(getApplicationContext(), "サービス実行中", "設定画面に移動します", pendingIntent);
+        //Intent intent = new Intent(this, Standby.class);
 
-        notificationManager.notify(0,notification);
-        int next = randomMessage.nextInt(message.length);
-        Toast.makeText(this,message[next],Toast.LENGTH_LONG).show();
+        //PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+
+        //notification.setLatestEventInfo(getApplicationContext(), "サービス実行中", "設定画面に移動します", pendingIntent);
+
+        //notificationManager.notify(0,notification);
+        //int next = randomMessage.nextInt(message.length);
+        //Toast.makeText(this,message[next],Toast.LENGTH_LONG).show();
+
+        //saikidou
+        return START_NOT_STICKY;
     }
 
     public void onDestroy(){
-        notificationManager.cancel(0);
+        //notificationManager.cancel(0);
+        manager.cancel(NOTIFICATION_MINIMUM_ID);
+
     }
 
     public IBinder onBind(Intent intent){
