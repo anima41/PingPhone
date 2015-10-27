88f07d20b3845707256872238584e4d5b60af807
tesoro01 <aqaqua01@gmail.com>
2015-10-27 21:47:33 +0900

    stopService修正

--- app/src/main/java/com/redandborder/pingphone/Standby.java
+++ app/src/main/java/com/redandborder/pingphone/Standby.java
@@ -53,6 +53,10 @@ public class Standby extends Activity implements OnClickListener {
     private Button callButton;
     private Button menuSetBtn;
 
+    private Intent intent = null;
+
+    private AlarmManager alarmManager =null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -135,13 +139,13 @@ public class Standby extends Activity implements OnClickListener {
             skypeCall(idText, this);
 
             Context context = getApplicationContext();
-            Intent intent = new Intent(context,MessageService.class);
+            intent = new Intent(context,MessageService.class);
             startService(intent);
 
-            PendingIntent pendingIntent = PendingIntent.getService(context,0,intent,0);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
 
-            AlarmManager alarmManager =(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
             long time = Calendar.getInstance().getTimeInMillis();
+            alarmManager =(AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
             alarmManager.setRepeating(
                     AlarmManager.RTC_WAKEUP,
                     time,5*1000,
@@ -256,6 +260,11 @@ public class Standby extends Activity implements OnClickListener {
         layout.setBackgroundColor(Color.WHITE);
         callButton.setBackground(btn_up);
 
+        if (alarmManager != null){
+            PendingIntent pendingIntent = PendingIntent.getService(this,0,intent,0);
+            alarmManager.cancel(pendingIntent);
+        }
+
     }
 
     @Override
@@ -267,8 +276,10 @@ public class Standby extends Activity implements OnClickListener {
             mTimer = null;
         }
 
-        Intent intent = new Intent(Standby.this,MessageService.class);
-        stopService(intent);
+        if (alarmManager != null){
+            PendingIntent pendingIntent = PendingIntent.getService(this,0,intent,0);
+            alarmManager.cancel(pendingIntent);
+        }
 
     }
 
