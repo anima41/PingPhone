eb736b9782b78f73be55e259e077488eb48309d3
tesoro01 <aqaqua01@gmail.com>
2015-10-27 20:26:53 +0900

    修正

--- app/src/main/java/com/redandborder/pingphone/service/MessageService.java
+++ app/src/main/java/com/redandborder/pingphone/service/MessageService.java
@@ -3,14 +3,10 @@ package com.redandborder.pingphone.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 
 public class MessageService extends Service {
-    NotificationManagerCompat manager = null;
-
-    private static final int NOTIFICATION_MINIMUM_ID = 001;
 
     public void onCreate() {
     }
@@ -18,12 +14,11 @@ public class MessageService extends Service {
     @Override
     public int onStartCommand(Intent it, int id, int startId) {
          Toast.makeText(getApplicationContext(), "Application", Toast.LENGTH_SHORT).show();
-        return NOTIFICATION_MINIMUM_ID;
+
+        //saikidou
+        return START_NOT_STICKY;
     }
 
-    public void onDestroy() {
-
-    }
 
     public IBinder onBind(Intent intent) {
         return null;
